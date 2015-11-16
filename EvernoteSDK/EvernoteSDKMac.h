18fdf6492ceab0646991c9a1467ec465b029677a
kimimaro <kimirius@gmail.com>
2015-11-16 17:43:57 +0800

    adapt to swift

---
+++ EvernoteSDK/EvernoteSDKMac.h
@@ -0,0 +1,13 @@
+//
+//  EvernoteSDKMac.h
+//  EvernoteSDK
+//
+//  Created by kimimaro on 15/11/16.
+//  Copyright © 2015年 n/a. All rights reserved.
+//
+
+#ifndef EvernoteSDKMac_h
+#define EvernoteSDKMac_h
+
+
+#endif /* EvernoteSDKMac_h */
--- SampleApp-Mac/AppDelegate.m
+++ SampleApp-Mac/AppDelegate.m
@@ -7,7 +7,7 @@
 //
 
 #import "AppDelegate.h"
-#import <EvernoteSDK-Mac/EvernoteSDK.h>
+#import <EvernoteSDKMac/EvernoteSDK.h>
 
 @implementation AppDelegate
 
