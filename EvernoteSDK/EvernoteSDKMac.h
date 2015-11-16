04c4e9330d0a917aca7747c2e45765ffb51eb9b2
kimimaro <kimirius@gmail.com>
2015-11-16 20:11:28 +0800

    fix umbrella file public

--- EvernoteSDK/EvernoteSDKMac.h
+++ EvernoteSDK/EvernoteSDKMac.h
@@ -10,5 +10,27 @@
 #define EvernoteSDKMac_h
 
 #import <EvernoteSDKMac/EvernoteSDK.h>
+//#import <EvernoteSDKMac/EDAMNoteStoreClient+Utilities.h>
+//#import <EvernoteSDKMac/ENConstants.h>
+//#import <EvernoteSDKMac/EvernoteNoteStore+Extras.h>
+//#import <EvernoteSDKMac/EvernoteNoteStore.h>
+//#import <EvernoteSDKMac/EvernoteUserStore+Extras.h>
+//#import <EvernoteSDKMac/EvernoteUserStore.h>
+//#import <EvernoteSDKMac/ENApplicationRequest.h>
+//#import <EvernoteSDKMac/ENApplicationBridge.h>
+//#import <EvernoteSDKMac/ENAuthenticationRequest.h>
+//#import <EvernoteSDKMac/ENCredentials.h>
+//#import <EvernoteSDKMac/ENCredentialStore.h>
+//#import <EvernoteSDKMac/ENLinkNotebookRequest.h>
+//#import <EvernoteSDKMac/ENMLUtility.h>
+//#import <EvernoteSDKMac/ENNewNoteRequest.h>
+//#import <EvernoteSDKMac/ENNoteImportRequest.h>
+//#import <EvernoteSDKMac/ENNoteViewRequest.h>
+//#import <EvernoteSDKMac/ENResourceAttachment.h>
+//#import <EvernoteSDKMac/ENSearchRequest.h>
+//#import <EvernoteSDKMac/NSDataBase64Additions.h>
+//#import <EvernoteSDKMac/NSDate+EDAMAdditions.h>
+//#import <EvernoteSDKMac/NSData+EvernoteSDK.h>
+
 
 #endif /* EvernoteSDKMac_h */
